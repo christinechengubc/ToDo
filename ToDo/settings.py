3a1456f180f7ec7efb207c5256895d1f0769c602
Christine <christine.lyn.cheng@gmail.com>
2018-01-05 11:50:43 -0800

    Added authentication templates

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ToDo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+# This logs any emails sent to the console (so you can copy the password reset link from the console).
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
