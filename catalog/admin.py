48b7c28fc36f7fe749371245917f386b9ebebba1
Daniel Lai <jujubix@gmail.com>
2018-01-06 12:46:20 -0800

    Updated loaddata.sh to current mode

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import TodoItem, TodoList, Profile
+from .models import TodoItem, TodoList, Profile, User
 
 admin.site.register(TodoItem)
 admin.site.register(TodoList)
